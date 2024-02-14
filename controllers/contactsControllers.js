55d9731fd0c3785862059e50c2419e773f202e83
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-02-14 21:10:36 +0100

    step 3: error handling is added for get controllers, post controller is added with joi schema for new contacts input validation is added either

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,22 +1,46 @@
 import contactsService from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
+import { createContactSchema } from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = async (req, res) => {
-  const contactsList = await contactsService.listContacts();
-  res.json(contactsList);
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contactsList = await contactsService.listContacts();
+    res.json(contactsList);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getOneContact = async (req, res) => {
-  const { id } = req.params;
-  const searchedContact = await contactsService.getContactById(id);
-  if (!searchedContact) {
-    throw HttpError(404);
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const searchedContact = await contactsService.getContactById(id);
+    if (!searchedContact) {
+      throw HttpError(404);
+    }
+    res.json(searchedContact);
+  } catch (error) {
+    next(error);
   }
-  res.json(searchedContact);
 };
 
 export const deleteContact = (req, res) => {};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400);
+    }
+    const newContact = await contactsService.addContact(
+      req.body.name,
+      req.body.email,
+      req.body.phone
+    );
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const updateContact = (req, res) => {};
