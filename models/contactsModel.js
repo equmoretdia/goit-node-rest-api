aad393877ece80e1ec752d125df86050b733010b
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-02-18 22:17:29 +0100

    Mongoose schema and model are added to models

---
+++ models/contactsModel.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+contactSchema.post("save", (error, data, next) => {
+  error.status = 400;
+  next();
+});
+
+const ContactsModel = model("contact", contactSchema);
+
+export default ContactsModel;
