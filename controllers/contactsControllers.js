c846cf9b14d8446c6bf55728f35b68102091c710
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-02-23 23:57:41 +0100

    ctrlWrapper helper is added, corresponding updates of controllers and routes are implemented

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,79 +1,69 @@
 import { ContactsModel } from "../models/contactsModel.js";
 import HttpError from "../helpers/HttpError.js";
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
 
-export const getAllContacts = async (req, res, next) => {
-  try {
-    const contactsList = await ContactsModel.find();
-    res.json(contactsList);
-  } catch (error) {
-    next(error);
-  }
+const getAllContacts = async (req, res) => {
+  const contactsList = await ContactsModel.find();
+  res.json(contactsList);
 };
 
-export const getOneContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const searchedContact = await ContactsModel.findById(id);
-    if (!searchedContact) {
-      throw HttpError(404);
-    }
-    res.json(searchedContact);
-  } catch (error) {
-    next(error);
+const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const searchedContact = await ContactsModel.findById(id);
+  if (!searchedContact) {
+    throw HttpError(404);
   }
+  res.json(searchedContact);
 };
 
-export const createContact = async (req, res) => {
+const createContact = async (req, res) => {
   const newContact = await ContactsModel.create(req.body);
   res.status(201).json(newContact);
 };
 
-export const updateContact = async (req, res, next) => {
-  try {
-    if (Object.keys(req.body).length === 0) {
-      throw HttpError(400, "Body must have at least one field");
-    }
-    const { id } = req.params;
-    const updatedContact = await ContactsModel.findByIdAndUpdate(id, req.body, {
+const updateContact = async (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    throw HttpError(400, "Body must have at least one field");
+  }
+  const { id } = req.params;
+  const updatedContact = await ContactsModel.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!updatedContact) {
+    throw HttpError(404);
+  }
+  res.json(updatedContact);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { id } = req.params;
+  const updatedContactStatus = await ContactsModel.findByIdAndUpdate(
+    id,
+    req.body,
+    {
       new: true,
-    });
-    if (!updatedContact) {
-      throw HttpError(404);
     }
-    res.json(updatedContact);
-  } catch (error) {
-    next(error);
+  );
+  if (!updatedContactStatus) {
+    throw HttpError(404);
   }
+  res.json(updatedContactStatus);
 };
 
-export const updateStatusContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const updatedContactStatus = await ContactsModel.findByIdAndUpdate(
-      id,
-      req.body,
-      {
-        new: true,
-      }
-    );
-    if (!updatedContactStatus) {
-      throw HttpError(404);
-    }
-    res.json(updatedContactStatus);
-  } catch (error) {
-    next(error);
+const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  const removedContact = await ContactsModel.findByIdAndDelete(id);
+  if (!removedContact) {
+    throw HttpError(404);
   }
+  res.json(removedContact);
 };
 
-export const deleteContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const removedContact = await ContactsModel.findByIdAndDelete(id);
-    if (!removedContact) {
-      throw HttpError(404);
-    }
-    res.json(removedContact);
-  } catch (error) {
-    next(error);
-  }
+export default {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getOneContact: ctrlWrapper(getOneContact),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+  deleteContact: ctrlWrapper(deleteContact),
 };
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,12 +1,5 @@
 import express from "express";
-import {
-  getAllContacts,
-  getOneContact,
-  createContact,
-  updateContact,
-  updateStatusContact,
-  deleteContact,
-} from "../controllers/contactsControllers.js";
+import contactsController from "../controllers/contactsControllers.js";
 import validateBody from "../helpers/validateBody.js";
 import isValidId from "../helpers/isValidId.js";
 import {
@@ -17,26 +10,30 @@ import {
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", getAllContacts);
+contactsRouter.get("/", contactsController.getAllContacts);
 
-contactsRouter.get("/:id", isValidId, getOneContact);
+contactsRouter.get("/:id", isValidId, contactsController.getOneContact);
 
-contactsRouter.post("/", validateBody(createContactSchema), createContact);
+contactsRouter.post(
+  "/",
+  validateBody(createContactSchema),
+  contactsController.createContact
+);
 
 contactsRouter.put(
   "/:id",
   isValidId,
   validateBody(updateContactSchema),
-  updateContact
+  contactsController.updateContact
 );
 
 contactsRouter.patch(
   "/:id/favorite",
   isValidId,
   validateBody(updateStatusContactSchema),
-  updateStatusContact
+  contactsController.updateStatusContact
 );
 
-contactsRouter.delete("/:id", isValidId, deleteContact);
+contactsRouter.delete("/:id", isValidId, contactsController.deleteContact);
 
 export default contactsRouter;
