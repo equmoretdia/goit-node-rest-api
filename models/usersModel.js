07012c4c4a84a7c39e814a19ea1515e1ec29eeec
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-02-24 21:47:27 +0100

    step 1: UsersModel is added, contactSchema is updated with owner property

---
+++ models/usersModel.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+import handleMongooseError from "../helpers/handleMongooseError.js";
+
+const subscriptionOptions = ["starter", "pro", "business"];
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: subscriptionOptions,
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const UsersModel = model("user", userSchema);
+
+export { UsersModel };
